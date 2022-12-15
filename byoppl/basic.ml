aa829afa2b7de1b9d5b8afb6c0e0e675c272dafb
Guillaume Baudart <guillaume.baudart@inria.fr>
2022-12-15 14:59:19 +0100

    Basic Infer

--- byoppl/basic.ml
+++ byoppl/basic.ml
@@ -1,15 +1,32 @@
 module Rejection_sampling = struct
-  let sample _ = assert false
-  let factor _ = assert false
-  let assume _ = assert false
-  let observe _ = assert false
-  let infer ?n:_n _ _ = assert false
+
+  type prob = Prob
+
+  exception Reject
+  let sample _prob d = Distribution.draw d
+  let factor _prob _ = assert false
+
+  let assume _prob p = if not p then raise Reject
+
+  let observe prob d v  = 
+    let y = sample prob d in
+    assume prob (y = v)
+  
+  let infer ?(n=1000) model data = 
+    let rec exec i = try model Prob data with Reject -> exec i in
+    let values = Array.init n exec in
+    Distribution.uniform_support ~values
 end
 
 module Importance_sampling = struct
-  let sample _ = assert false
-  let factor _ = assert false
-  let assume _ = assert false
-  let observe _ = assert false
-  let infer ?n:_n _ _ = assert false
+  type prob = {id: int; scores: float Array.t}
+
+  let sample _prob d = Distribution.draw d
+  let factor prob s = prob.scores.(prob.id) <- prob.scores.(prob.id) +. s
+  let assume prob p = factor prob (if p then 0. else -. infinity)
+  let observe prob d v = factor prob (Distribution.logpdf d v)
+  let infer ?(n=1000) model data = 
+    let scores = Array.make n 0. in
+    let values = Array.mapi (fun i _ -> model { id=i; scores } data) scores in
+    Distribution.support ~values ~logits:scores
 end
--- byoppl/infer.ml
+++ byoppl/infer.ml
@@ -1,25 +1,85 @@
 module Gen = struct
-  type 'a prob = TODO
+  type 'a prob = 'a option
   and 'a next = 'a prob -> 'a prob
   and ('a, 'b) model = 'a -> ('b -> 'b next) -> 'b next
 
-  let sample _ = assert false
-  let factor _ = assert false
-  let assume _ = assert false
-  let observe _ _ = assert false
-  let draw _ _ = assert false
+  let sample d k prob = 
+    let v = Distribution.draw d in
+    k v prob
+
+  let factor _s k prob =
+    k () prob
+
+  let assume p = factor (if p then 0. else -. infinity)
+  let observe d v = factor (Distribution.logpdf d v)
+
+  let exit v _prob = Some v
+
+  let draw model data = 
+    let v = model data exit None in
+    Option.get v
 end
 
 module Importance_sampling = struct
-  type 'a prob = TODO
+  type 'a prob = {id: int; particles : 'a particle Array.t }
+  and 'a particle = {value: 'a option; score : float; k: 'a next}
   and 'a next = 'a prob -> 'a prob
   and ('a, 'b) model = 'a -> ('b -> 'b next) -> 'b next
 
-  let sample _ = assert false
-  let factor _ = assert false
-  let assume _ = assert false
-  let observe _ _ = assert false
-  let infer ?n:_n _ _ = assert false
+  let sample d k prob = 
+    let v = Distribution.draw d in
+    k v prob
+
+  let factor s k prob = 
+    let particle = prob.particles.(prob.id) in
+    prob.particles.(prob.id) <- {particle with score = s +. particle.score };
+    k () prob
+    
+  let assume p = factor (if p then 0. else -. infinity)
+  let observe d v = factor (Distribution.logpdf d v)
+
+  let run_next prob = 
+    if prob.id < Array.length prob.particles - 1 then
+      let k = prob.particles.(prob.id + 1).k in
+      k {prob with id = prob.id + 1}
+    else
+      prob
+
+  let exit v prob = 
+    let particle = prob.particles.(prob.id) in
+    prob.particles.(prob.id) <- {particle with value = Some v};
+    run_next prob
+
+  let infer ?(n=1000) model data =
+    let init_particles = {value = None; score = 0.; k = (model data) exit } in
+    let prob = {id = -1; particles = Array.make n init_particles } in
+    let prob = run_next prob in
+    let values = Array.map (fun p -> Option.get p.value) prob.particles in
+    let scores = Array.map (fun p -> p.score) prob.particles in
+    Distribution.support ~values ~logits:scores
 end
 
-module Particle_filter = struct end
+module Particle_filter = struct 
+  include Importance_sampling
+
+  let resample particles = 
+    let logits = Array.map (fun p -> p.score) particles in
+    let values = Array.map (fun p -> {p with score = 0.}) particles in
+    let dist = Distribution.support ~values ~logits in
+    Array.init (Array.length particles) (fun _ -> Distribution.draw dist)
+
+
+  let factor s k prob = 
+    let particle = prob.particles.(prob.id) in
+    prob.particles.(prob.id) <- 
+      {particle with score = s +. particle.score; k = k () };
+    let prob = 
+      if prob.id < Array.length prob.particles - 1 then prob
+      else { id = -1; particles = resample prob.particles }
+    in
+    run_next prob
+
+  let assume p = factor (if p then 0. else -. infinity)
+  let observe d v = factor (Distribution.logpdf d v)
+
+end
