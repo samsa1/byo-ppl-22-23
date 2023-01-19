5db13ae8b252585e91e30f0fcba3236842ee8bb2
Guillaume Baudart <guillaume.baudart@inria.fr>
2023-01-19 14:15:43 +0100

    Metropolis Hasting

--- byoppl/infer.ml
+++ byoppl/infer.ml
@@ -83,3 +83,99 @@ module Particle_filter = struct
   let observe d v = factor (Distribution.logpdf d v)
 
 end
+
+module Metropolis_hasting = struct
+  type 'a prob = {
+    id : int;
+    trace : 'a particle list;
+    current_score : float;
+    regen_from : int;
+    old_trace : 'a particle list;
+    old_score : float;
+    old_val : 'a option;
+    values : 'a option array;
+  }
+
+  and 'a particle =
+    | Particle : {
+        k : 'b -> 'a next;
+        score : float;
+        value : 'b;
+        dist : 'b Distribution.t;
+      }
+        -> 'a particle
+
+  and 'a next = 'a prob -> 'a prob
+  and ('a, 'b) model = 'a -> ('b -> 'b next) -> 'b next
+
+  let sample dist k prob =
+    let value = Distribution.draw dist in
+    let particle = Particle { k; score = prob.current_score; value; dist } in
+    k value
+      {
+        prob with
+        trace = particle :: prob.trace;
+        current_score = prob.current_score;
+      }
+
+  let factor s k prob =
+    k () { prob with current_score = prob.current_score +. s }
+
+  let assume p = factor (if p then 0. else -.infinity)
+  let observe d x = factor (Distribution.logpdf d x)
+
+  let accept prob =
+    match prob.old_val with
+    | None -> 1.
+    | Some _ ->
+        let n_old = -.log (prob.old_trace |> List.length |> Float.of_int) in
+        let n_cur = -.log (prob.trace |> List.length |> Float.of_int) in
+        min 1. (exp (prob.current_score -. prob.old_score +. n_cur -. n_old))
+
+  let exit v prob =
+    if prob.id < Array.length prob.values - 1 then (
+      let prob = { prob with id = prob.id + 1 } in
+      let v, prob =
+        if Distribution.(draw(bernoulli ~p:(accept prob))) = 0 then
+          ( Option.get prob.old_val,
+            { prob with current_score = prob.old_score; trace = prob.old_trace }
+          )
+        else (v, prob)
+      in
+      prob.values.(prob.id) <- Some v;
+      let regen_from = Random.int (List.length prob.trace) in
+      let (Particle regen) = List.nth prob.trace regen_from in
+      let prob =
+        {
+          prob with
+          trace = Utils.slice prob.trace regen_from;
+          current_score = regen.score;
+          regen_from;
+          old_trace = prob.trace;
+          old_val = Some v;
+          old_score = prob.current_score;
+        }
+      in
+      sample regen.dist regen.k prob)
+    else prob
+
+  let infer ?(n = 1000) ?(warmup = n) m data =
+    let prob =
+      (m data) exit
+        {
+          id = -1;
+          trace = [];
+          current_score = 0.;
+          regen_from = 0;
+          old_trace = [];
+          old_val = None;
+          old_score = -.infinity;
+          values = Array.make (warmup + n) None;
+        }
+    in
+    let values =
+      prob.values |> Array.map Option.get |> fun a ->
+      Array.sub a warmup n
+    in
+    Distribution.uniform_support ~values
+end
--- examples/hmm.ml
+++ examples/hmm.ml
@@ -70,3 +70,28 @@ let _ =
   let dist = Distribution.split_list (infer hmm data) in
   let m_x = List.map Distribution.mean (List.rev dist) in
   List.iter2 (Format.printf "%f %f@.") data m_x
+
+open Cps_operators
+open Infer.Metropolis_hasting
+
+let hmm data =
+  let rec gen states data =
+    match (states, data) with
+    | [], y :: data -> gen [ y ] data
+    | states, [] -> return states
+    | pre_x :: _, y :: data ->
+        let* x = sample (gaussian ~mu:pre_x ~sigma:1.0) in
+        let* () = observe (gaussian ~mu:x ~sigma:1.0) y in
+        gen (x :: states) data
+  in
+  gen [] data
+
+let _ =
+  Format.printf "@.-- HMM, CPS MCMC --@.";
+  let data =
+    Owl.Arr.(linspace 0. 20. 20 + gaussian [| 20 |])
+    |> Owl.Arr.to_array |> Array.to_list
+  in
+  let dist = Distribution.split_list (infer hmm data) in
+  let m_x = List.map Distribution.mean (List.rev dist) in
+  List.iter2 (Format.printf "%f %f@.") data m_x
